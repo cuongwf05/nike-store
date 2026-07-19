8295b2831ef92e9046b6285f90262ae25b7fcd6d
Nguyen Trong Nhan <nhan.2375106052637@vanglanguni.vn>
2026-07-19 16:29:46 +0700

    trending section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Trending from "@/components/Trending";
 
 export default function Home() {
   return (
-    <div >
-      <main >
-        <Header></Header>
-        <Hero></Hero>
-      </main>
-    </div>
+    <main >
+      <Header></Header>
+      <Hero></Hero>
+      <Trending></Trending>
+    </main>
   );
 }
